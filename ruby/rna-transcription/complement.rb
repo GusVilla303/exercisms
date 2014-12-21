72c27c31091766a83ddd7ab724368b54f375251a
GusVilla303 <gusvilla303@gmail.com>
2014-12-20 18:56:03 -0700

    Complement exercise, DNA portion complete, need to do RNA portion

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -1,10 +1,18 @@
 class Complement
-  def self.of_dna(nucleotide)
-    nucleotide.tr('GCTA', 'CGAU')
-  end
+  @nucleotide_lookup = {
+    'G' => 'C',
+    'C' => 'G',
+    'T' => 'A',
+    'A' => 'U'
+  }
 
-  def self.of_rna(nucleotide)
-    nucleotide.tr('CGAU', 'GCTA')
+  def self.of_dna(sequence)
+    rna_complement = ''
+
+    sequence.chars.each do |nucleotide|
+      rna_complement << @nucleotide_lookup[nucleotide].to_s
+    end
+    rna_complement
   end
 
 end
