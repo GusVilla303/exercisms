12c42e72fda6b77c5a156afd60c4b36eb5a74c09
GusVilla303 <gusvilla303@gmail.com>
2015-01-25 21:15:45 -0700

    Add NUMERALS collection to roman.rb file

--- ruby/roman-numerals/roman.rb
+++ ruby/roman-numerals/roman.rb
@@ -1,10 +1,28 @@
 class Roman
 
   NUMERALS = {
-    ""
+    'MMM'    => 3000,
+    'MXXIV'  => 1024,
+    'CMXI'   => 911,
+    'DLXXV'  => 575,
+    'CDII'   => 402,
+    'CLXIII' => 163,
+    'CXLI'   => 141,
+    'XCIII'  => 93,
+    'LIX'    => 59,
+    'XLVIII' => 48,
+    'XXVII'  => 27,
+    'IX'     => 9,
+    'VI'     => 6,
+    'V'      => 5,
+    'IV'     => 4,
+    'III'    => 3,
+    'II'     => 2,
+    'I'      => 1
   }
 
   def to_roman
+    number = ''
 
   end
 
