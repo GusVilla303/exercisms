21fadaa2b7c3ec276ffd7b8628c6b817bfedccd5
GusVilla303 <gusvilla303@gmail.com>
2015-01-30 17:06:10 -0700

    Implement fourth iteration by using String#tr on sequence. All tests are passing.

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -1,16 +1,9 @@
 class Complement
-  NUCLEOTIDES = {
-                  'G' => 'C',
-                  'C' => 'G',
-                  'T' => 'A',
-                  'A' => 'U'
-                }
-
   def self.of_dna(sequence)
-    sequence.chars.map { |e| NUCLEOTIDES[e] }.join
+    sequence.tr('GCTA', 'CGAU')
   end
 
   def self.of_rna(sequence)
-    sequence.chars.map { |e| NUCLEOTIDES.key(e) }.join
+    sequence.tr('CGAU', 'GCTA')
   end
 end
