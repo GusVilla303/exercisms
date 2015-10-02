d1264632c7b2ff941810ce574311bc7696ddadb6
GusVilla303 <gusvilla303@gmail.com>
2015-10-02 14:29:02 -0600

    Iteration 3: Use function Object Constructor

--- javascript/hello-world/hello-world.js
+++ javascript/hello-world/hello-world.js
@@ -4,11 +4,11 @@
 // Make sure to look at test.script.js--that should give you some hints about what is
 // expected here.
 
-var HelloWorld = function() {};
-
-HelloWorld.prototype.hello = function(input) {
+function HelloWorld() {
+    this.hello = function(input) {
     input = input || "world";
     return "Hello, " + input + "!";
+  }
 };
 
 module.exports = HelloWorld;
